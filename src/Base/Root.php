e4d04cb95c7f3859b5b38004cbc9522207c9f01b
Pramod <prkrim@gmail.com>
2019-01-18 19:26:05 +0530

    Initial Setup

---
+++ src/Base/Root.php
@@ -0,0 +1,19 @@
+<?php
+namespace OLP\Base;
+/**
+  * Contains all methods related to disk directories.
+  *
+  * @author  Pramod Kumar <pramod@zonalhost.com>
+  *
+  * @since 1.0
+  *
+  */
+
+class Root
+{
+	
+	public function __construct()
+	{
+		# code...
+	}
+}
---
+++ src/Storage/Dir.php
@@ -0,0 +1,20 @@
+<?php
+namespace OLP\Storage;
+/**
+  * Contains all methods related to disk directories.
+  *
+  * @author  Pramod Kumar <pramod@zonalhost.com>
+  *
+  * @since 1.0
+  *
+  * @param int    $example  This is an example function/method parameter description.
+  * @param string $example2 This is a second example.
+  */
+class Dir extends Root
+{
+	
+	public function __construct()
+	{
+		# code...
+	}
+}
